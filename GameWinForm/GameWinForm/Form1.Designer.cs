db3b429d6b7c5fe6c474fd96cf7623a034db5e6d
eseosaO <eseosaoshodin@yahoo.com>
2020-06-13 16:18:35 +0100

    Created a Windows Form for Games' shop

---
+++ GameWinForm/GameWinForm/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace GameWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gameTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // gameTextBox
+            // 
+            this.gameTextBox.Location = new System.Drawing.Point(80, 29);
+            this.gameTextBox.Multiline = true;
+            this.gameTextBox.Name = "gameTextBox";
+            this.gameTextBox.Size = new System.Drawing.Size(464, 214);
+            this.gameTextBox.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(956, 450);
+            this.Controls.Add(this.gameTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox gameTextBox;
+    }
+}
+
